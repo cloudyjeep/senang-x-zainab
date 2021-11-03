2d4ab98b125fd873cd45a75d17e1a74b071a00fd
Razif <cloudyjeep@gmail.com>
2021-11-03 18:54:38 +0700

    component <Button/>

---
+++ src/component/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  margin-right: 8px;
+  border: 0;
+  border-radius: 12px;
+  padding: 12px 18px;
+  font-size: 16px;
+  cursor: pointer
+`;
+
+export default Button
