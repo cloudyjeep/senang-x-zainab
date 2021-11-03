4f4a7f4f2da39f47852b19aa5816a0debb7d0971
Razif <cloudyjeep@gmail.com>
2021-11-03 18:55:30 +0700

    component Input

---
+++ src/component/Input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const InputText = styled.input`
+  padding: 12px 14px;
+  border-radius: 6px;
+  margin-bottom: 12px;
+  border: 1px solid #d1d1d1;
+  font-size: 16px;
+`;
